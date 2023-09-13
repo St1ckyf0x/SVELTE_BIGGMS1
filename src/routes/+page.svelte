79ed049907ca7eb6b41772384e5207ea5be4405d
Matthew Biggs <BIGGMS1@student.op.ac.nz>
2023-09-13 12:55:41 +1200

    added an image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
-<h1>Dork and Dorker</h1>
-
+<h1 class="text">Brighton MMA Gym</h1>
+<div class = "mma-image1">
+    <img src ="/IMG/pexels-pavel-danilyuk-6296063.jpg" alt = "FRONTIMAGE" width="1000px">
+    <!-- <style="width: 100px;"></style> -->
+</div>
 
 
