1d34a7d3a7121ae65fcec28a0811e6b34bacfd60
Matthew Biggs <BIGGMS1@student.op.ac.nz>
2023-10-18 16:56:02 +1300

    added text and opacity for the background-text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,10 @@
     <img src ="/IMG/pexels-pavel-danilyuk-6296063.jpg" alt = "FRONTIMAGE" width="2600x">
     <!-- <style="width: 100px;"></style> -->
 </div>
+<div class= "home-page-text">
+    <h1>
+        WELCOME to Brighton MMA Gym, blah blah sign up
+    </h1>
+</div>
 
 
